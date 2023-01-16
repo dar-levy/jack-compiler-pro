389a30fa7c72fd48e32a89978b0d8105f586a26f
Dar <darlevy8@gmail.com>
2023-01-16 11:09:16 +0200

    Remove redundant comments

--- JackTokenizer.py
+++ JackTokenizer.py
@@ -22,9 +22,6 @@ class JackTokenizer:
     IDENTIFIER = 4
 
     def __init__(self, input_file_path):
-        """
-        :param input_file: the current file
-        """
         with open(input_file_path, "r") as file:
             self.text = file.read()
         self._clear_all_comments()
@@ -32,9 +29,6 @@ class JackTokenizer:
         self._currentToken = None
 
     def _clear_all_comments(self):
-        """
-        Clear all comments from self.text .
-        """
         self.text = re.sub(COMMENT, "", self.text)
 
     def hasMoreTokens(self):
