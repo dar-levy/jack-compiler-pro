2f01c01833f5561b40abe5f40f3fc0943c654d64
Dar Levy <darlevy8@gmail.com>
2023-01-22 19:24:25 +0200

    Add double to patterns

--- Patterns.py
+++ Patterns.py
@@ -5,7 +5,7 @@ COMMENT = "(//.*)|(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)"
 EMPTY_TEXT = re.compile("\s*")
 KEY_WORD = re.compile("^\s*("
                       "class|constructor|function|method|static|field"
-                      "|var|int|char|boolean|void|true|false|null|this|"
+                      "|var|int|char|boolean|double|void|true|false|null|this|"
                       "let|do|if|else|while|return)\s*")
 SYMBOL = re.compile("^\s*([{}()\[\].,;+\-*/&|<>=~])\s*")
 DIGIT = re.compile("^\s*(\d+)\s*")
